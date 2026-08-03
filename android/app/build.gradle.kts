c21194f6dcce5e10ae7e3342a887d0de4aae92b5
Kanishk Pachauri <itskanishkp.py@gmail.com>
2026-08-04 01:27:12 +0530

    build: bump Android release to 0.1.0-beta.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.mrsunglasses.localflow"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0-beta.2"
+        versionCode = 3
+        versionName = "0.1.0-beta.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
